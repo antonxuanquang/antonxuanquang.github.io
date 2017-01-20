2c74d69c14bd911874d3d85d65dcb82c5cd0659d
antonxuanquang <antonxuanquang@yahoo.com>
2017-01-20 09:28:31 -0600

    Profile

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $('i.fa').tooltip();
 $('.page-scroll a').on('click', function(event) {
   var $anchor = $(this);
   $('html, body').stop().animate({
-    scrollTop: $($anchor.attr('href')).offset().top
+    scrollTop: $($anchor.attr('href')).offset().top - 70
   }, 1500, 'easeInOutExpo');
   event.preventDefault();
 });
